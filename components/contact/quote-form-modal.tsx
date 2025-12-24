7e0a5d339d99f0e02500228fb8834b273d6522eb
thongho <thongho1512@gmail.com>
2025-12-25 00:12:23 +0700

    change time1

--- components/contact/quote-form-modal.tsx
+++ components/contact/quote-form-modal.tsx
@@ -54,7 +54,7 @@ export default function QuoteFormModal() {
           localStorage.setItem(FORM_SHOWN_KEY, "true")
           setIsOpen(true)
         }
-      }, 60000) // 3 phút
+      }, 30000) // 3 phút
 
       return () => clearTimeout(timer)
     } else {
